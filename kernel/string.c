c049f2cfa80c37398a3c57d801ceb4a6b3b71a0c
traitimtrongvag <pan2512811@gmail.com>
2026-02-11 05:52:25 +0000

    refactor: add null pointer checks to string functions

--- kernel/string.c
+++ kernel/string.c
@@ -1,27 +1,36 @@
 #include "string.h"
 
 void* memset(void* ptr, int value, size_t num) {
+    if (!ptr || num == 0) {
+        return ptr;
+    }
     unsigned char* p = ptr;
-    while(num--) {
+    while (num--) {
         *p++ = (unsigned char)value;
     }
     return ptr;
 }
 
 void* memcpy(void* dest, const void* src, size_t n) {
+    if (!dest || !src || n == 0) {
+        return dest;
+    }
     unsigned char* d = dest;
     const unsigned char* s = src;
-    while(n--) {
+    while (n--) {
         *d++ = *s++;
     }
     return dest;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
+    if (!s1 || !s2) {
+        return 0;
+    }
     const unsigned char* p1 = s1;
     const unsigned char* p2 = s2;
-    while(n--) {
-        if(*p1 != *p2) {
+    while (n--) {
+        if (*p1 != *p2) {
             return *p1 - *p2;
         }
         p1++;
@@ -31,13 +40,19 @@ int memcmp(const void* s1, const void* s2, size_t n) {
 }
 
 char* strcpy(char* dest, const char* src) {
+    if (!dest || !src) {
+        return dest;
+    }
     char* d = dest;
-    while((*d++ = *src++));
+    while ((*d++ = *src++));
     return dest;
 }
 
 int strcmp(const char* s1, const char* s2) {
-    while(*s1 && (*s1 == *s2)) {
+    if (!s1 || !s2) {
+        return 0;
+    }
+    while (*s1 && (*s1 == *s2)) {
         s1++;
         s2++;
     }
