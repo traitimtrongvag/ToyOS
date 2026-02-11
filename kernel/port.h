5d569b498dd8f4593e564370553d6d90660cebad
traitimtrongvag <pan2512811@gmail.com>
2026-02-11 05:53:49 +0000

    fix: add memory barriers to I/O operations to prevent reordering

--- kernel/port.h
+++ kernel/port.h
@@ -4,17 +4,17 @@
 #include <stdint.h>
 
 static inline void outb(uint16_t port, uint8_t val) {
-    __asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+    __asm__ volatile("outb %0, %1" : : "a"(val), "Nd"(port) : "memory");
 }
 
 static inline uint8_t inb(uint16_t port) {
     uint8_t ret;
-    __asm__ volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    __asm__ volatile("inb %1, %0" : "=a"(ret) : "Nd"(port) : "memory");
     return ret;
 }
 
 static inline void io_wait(void) {
-    outb(0x80, 0);
+    __asm__ volatile("outb %%al, $0x80" : : "a"(0) : "memory");
 }
 
 #endif
