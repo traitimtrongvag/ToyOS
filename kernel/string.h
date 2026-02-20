ea24836126415d5eb8949336ea740acb6d1aecfd
traitimtrongvag <pan2512811@gmail.com>
2026-02-20 11:26:32 +0000

    fix: add memmove declaration to string.h

--- kernel/string.h
+++ kernel/string.h
@@ -6,6 +6,7 @@
 
 void* memset(void* ptr, int value, size_t num);
 void* memcpy(void* dest, const void* src, size_t n);
+void* memmove(void* dest, const void* src, size_t n);
 int memcmp(const void* s1, const void* s2, size_t n);
 size_t strlen(const char* str);
 char* strcpy(char* dest, const char* src);
