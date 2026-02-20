f888d3086dcd2846abe0a67b610c0bb4c828c045
traitimtrongvag <pan2512811@gmail.com>
2026-02-20 11:26:29 +0000

    fix: remove duplicate mem funcs, keep memmove/bcmp

--- kernel/memory_funcs.c
+++ kernel/memory_funcs.c
@@ -1,50 +1,21 @@
 #include <stddef.h>
 #include <stdint.h>
-
-void* memcpy(void* dest, const void* src, size_t n) {
-    uint8_t* d = (uint8_t*)dest;
-    const uint8_t* s = (const uint8_t*)src;
-    for (size_t i = 0; i < n; i++) {
-        d[i] = s[i];
-    }
-    return dest;
-}
-
-void* memset(void* s, int c, size_t n) {
-    uint8_t* p = (uint8_t*)s;
-    for (size_t i = 0; i < n; i++) {
-        p[i] = (uint8_t)c;
-    }
-    return s;
-}
-
-int memcmp(const void* s1, const void* s2, size_t n) {
-    const uint8_t* p1 = (const uint8_t*)s1;
-    const uint8_t* p2 = (const uint8_t*)s2;
-    for (size_t i = 0; i < n; i++) {
-        if (p1[i] != p2[i]) {
-            return p1[i] - p2[i];
-        }
-    }
-    return 0;
-}
-
-int bcmp(const void* s1, const void* s2, size_t n) {
-    return memcmp(s1, s2, n) != 0;
-}
+#include "string.h"
 
 void* memmove(void* dest, const void* src, size_t n) {
     uint8_t* d = (uint8_t*)dest;
     const uint8_t* s = (const uint8_t*)src;
-    
+
     if (d < s) {
-        for (size_t i = 0; i < n; i++) {
+        for (size_t i = 0; i < n; i++)
             d[i] = s[i];
-        }
     } else {
-        for (size_t i = n; i > 0; i--) {
-            d[i-1] = s[i-1];
-        }
+        for (size_t i = n; i > 0; i--)
+            d[i - 1] = s[i - 1];
     }
     return dest;
+}
+
+int bcmp(const void* s1, const void* s2, size_t n) {
+    return memcmp(s1, s2, n) != 0;
 }
