2aca4b7ba2e76a342006cd8904d3aeb241a35a4f
traitimtrongvag <pan2512811@gmail.com>
2026-04-26 15:29:22 +0000

    feat(string): add strncmp, strncpy, strnlen declarations

--- kernel/string.h
+++ kernel/string.h
@@ -7,9 +7,12 @@
 void* memset(void* ptr, int value, size_t num);
 void* memcpy(void* dest, const void* src, size_t n);
 void* memmove(void* dest, const void* src, size_t n);
-int memcmp(const void* s1, const void* s2, size_t n);
+int   memcmp(const void* s1, const void* s2, size_t n);
 size_t strlen(const char* str);
+size_t strnlen(const char* str, size_t max);
 char* strcpy(char* dest, const char* src);
-int strcmp(const char* s1, const char* s2);
+char* strncpy(char* dest, const char* src, size_t n);
+int   strcmp(const char* s1, const char* s2);
+int   strncmp(const char* s1, const char* s2, size_t n);
 
 #endif
