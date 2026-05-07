529822d8c48c73a47b1963e563f4fc4a9aeb3ffe
pan2512811 <pan2512811@gmail.com>
2026-05-08 02:36:35 +0700

    refactor(memory): rename page_t to page_entry_t, add paging constants
    - rename page_t -> page_entry_t for clarity
    - add PAGE_TABLE_ENTRIES, PAGE_DIR_ENTRIES, PAGE_DIR_SHIFT, PAGE_TABLE_SHIFT
    - add PAGE_ADDR_MASK, PAGE_OFFSET_MASK, PAGE_TABLE_INDEX_MASK

--- kernel/memory.h
+++ kernel/memory.h
@@ -3,12 +3,21 @@
 
 #include <stdint.h>
 
-#define KERNEL_VIRTUAL_BASE 0xC0000000
-#define KERNEL_PAGE_NUMBER (KERNEL_VIRTUAL_BASE >> 22)
+#define KERNEL_VIRTUAL_BASE     0xC0000000U
+#define KERNEL_PAGE_NUMBER      (KERNEL_VIRTUAL_BASE >> 22)
 
-#define PAGE_SIZE 4096
-#define PAGE_ALIGN_DOWN(x) ((x) & ~(PAGE_SIZE - 1))
-#define PAGE_ALIGN_UP(x) (((x) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
+#define PAGE_SIZE               4096U
+#define PAGE_TABLE_ENTRIES      1024U
+#define PAGE_DIR_ENTRIES        1024U
+
+#define PAGE_ADDR_MASK          0xFFFFF000U
+#define PAGE_OFFSET_MASK        0x00000FFFU
+#define PAGE_DIR_SHIFT          22U
+#define PAGE_TABLE_SHIFT        12U
+#define PAGE_TABLE_INDEX_MASK   0x3FFU
+
+#define PAGE_ALIGN_DOWN(x)      ((x) & ~(PAGE_SIZE - 1))
+#define PAGE_ALIGN_UP(x)        (((x) + PAGE_SIZE - 1) & ~(PAGE_SIZE - 1))
 
 typedef struct {
     uint32_t present    : 1;
@@ -18,16 +27,16 @@ typedef struct {
     uint32_t dirty      : 1;
     uint32_t unused     : 7;
     uint32_t frame      : 20;
-} page_t;
+} page_entry_t;
 
 typedef struct {
-    page_t pages[1024];
+    page_entry_t entries[PAGE_TABLE_ENTRIES];
 } page_table_t;
 
 typedef struct {
-    uint32_t tables_physical[1024];
-    page_table_t* tables[1024];
-    uint32_t physical_addr;
+    uint32_t     tables_physical[PAGE_DIR_ENTRIES];
+    page_table_t *tables[PAGE_DIR_ENTRIES];
+    uint32_t     physical_addr;
 } page_directory_t;
 
 #endif
