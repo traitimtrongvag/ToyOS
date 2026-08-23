6b567e3885852eaf5c49d104fa4dc3cd3acd39d5
pan2512811 <pan2512811@gmail.com>
2026-08-23 16:45:00 +0700

    fix(string): add strlen implementation to string.c

--- kernel/string.c
+++ kernel/string.c
@@ -58,3 +58,10 @@ int strcmp(const char* s1, const char* s2) {
     }
     return *(unsigned char*)s1 - *(unsigned char*)s2;
 }
+
+size_t strlen(const char* str) {
+    size_t len = 0;
+    while (str[len])
+        len++;
+    return len;
+}
